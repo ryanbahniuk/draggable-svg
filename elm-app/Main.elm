4e6021e9176565eb2423f77e66b962b9887f9fc0
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2017-09-30 15:09:56 -0500

    upgrading to elm 0.18

--- elm-app/Main.elm
+++ elm-app/Main.elm
@@ -1,6 +1,6 @@
 module Main exposing (..)
 
-import Html.App
+import Html
 import Messages exposing (Msg(..))
 import Models exposing (Model, initialModel)
 import Update exposing (update)
@@ -12,7 +12,7 @@ init =
   ( initialModel, Cmd.none )
 
 main =
-  Html.App.program
+  Html.program
   { init = init
   , view = view
   , update = update
